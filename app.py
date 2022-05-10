9fa7321095b342d53a66e2561c8cbcd982c6ee3d
AriseTran <artran@csumb.edu>
2022-05-09 22:21:51 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def view_home():
+    return render_template("main.html", title="Home page")
+
+@app.route("/time-managment")
+def view_time_management():
+    return render_template("sample.html", title="Time Management")
+
+@app.route("/mental-strain")
+def view_mental_strain():
+    return render_template("sample.html", title="Mental Strain")
+    
+@app.route("/finacials")
+def view_finacials():
+    return render_template("sample.html", title="Finacials")
+    
+@app.route("/credit")
+def view_credit():
+    return render_template("sample.html", title="Credit")
+    
